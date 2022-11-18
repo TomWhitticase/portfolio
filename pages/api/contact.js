0f487938c89d90238071d8a105cae2dc5ae80d2b
TomWhitticase <79092168+TomWhitticase@users.noreply.github.com>
2022-11-18 16:42:29 +0000

    changes

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,13 +2,11 @@ const nodemailer = require("nodemailer");
 
 export default async (req, res) => {
   const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
+    service: "hotmail",
     auth: {
-      user: "passivesoybean@gmail.com",
-      pass: "thmagkmxqecyozln",
+      user: "tom.pe.wh.2@outlook.com",
+      pass: "8A7vGUJG",
     },
-    secure: true,
   });
 
   await new Promise((resolve, reject) => {
@@ -25,7 +23,7 @@ export default async (req, res) => {
   });
 
   const mailData = {
-    from: "passivesoybean@gmail.com",
+    from: "tom.pe.wh.2@outlook.com",
     to: "tom.pe.wh@outlook.com",
     subject: `Portfolio Website Message From ${req.body.name}`,
     text: req.body.message + " | Sent from: " + req.body.email,
@@ -49,32 +47,3 @@ export default async (req, res) => {
 
   res.status(200).json({ status: "OK" });
 };
-
-// export default function (req, res) {
-
-//   let nodemailer = require("nodemailer");
-
-//   const transporter = nodemailer.createTransport({
-//     port: 465,
-//     host: "smtp.gmail.com",
-//     auth: {
-//       user: "passivesoybean@gmail.com",
-//       pass: "thmagkmxqecyozln",
-//     },
-//     secure: true,
-//   });
-//   const mailData = {
-//     from: "passivesoybean@gmail.com",
-//     to: "tom.pe.wh@outlook.com",
-//     subject: `Portfolio Website Message From ${req.body.name}`,
-//     text: req.body.message + " | Sent from: " + req.body.email,
-//     html: `<div>${req.body.message}</div><p>Name:
-//     ${req.body.name}</p><p>Email:
-//     ${req.body.email}</p>`,
-//   };
-//   transporter.sendMail(mailData, function (err, info) {
-//     if (err) console.log(err);
-//     else console.log(info);
-//   });
-//   res.status(200).json({});
-// }
