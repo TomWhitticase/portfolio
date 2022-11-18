03d6d2ed9aedbe5a048de3ffa7cb9f8604fbe67f
TomWhitticase <79092168+TomWhitticase@users.noreply.github.com>
2022-11-18 19:39:10 +0000

    loading button

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -14,6 +14,8 @@ export default function Contact() {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
 
+  const [loading, setLoading] = useState(false);
+
   const handleEmailChange = (ev: any) => {
     setEmail(ev.target.value);
   };
@@ -26,6 +28,7 @@ export default function Contact() {
 
   const sendEmail = async (e: any) => {
     e.preventDefault();
+    setLoading(true);
 
     let data = {
       name,
@@ -43,6 +46,7 @@ export default function Contact() {
     }).then((res) => {
       console.log("Response received");
       if (res.status === 200) {
+        setLoading(false);
         toast.success("Message sent!", {
           position: "top-center",
           autoClose: 5000,
@@ -57,6 +61,7 @@ export default function Contact() {
         setEmail("");
         setMessage("");
       } else {
+        setLoading(false);
         toast.error("Something went wrong!", {
           position: "top-center",
           autoClose: 5000,
@@ -125,9 +130,39 @@ export default function Contact() {
           ></FontAwesomeIcon>
         </div>
         <div>
-          <button className="btn-1" type="submit">
-            Send <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
-          </button>
+          {loading ? (
+            <button
+              type="button"
+              className="btn-1 flex items-center w-44"
+              disabled={true}
+            >
+              <svg
+                className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Sending...
+            </button>
+          ) : (
+            <button className="btn-1 w-44" type="submit">
+              Send <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+            </button>
+          )}
         </div>
       </form>
     </div>
